c9f91eab9c66a8afc91d6fc7b1bd9ead14c773fe
Доллар Музыка <dollarmudic@gmail.com>
2019-07-11 13:32:44 +0600

    Proxy

--- DpHW3/Program.cs
+++ DpHW3/Program.cs
@@ -6,10 +6,63 @@ using System.Threading.Tasks;
 
 namespace DpHW3
 {
-    class Program
+    class MainApp
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
+
+            MathProxy proxy = new MathProxy();
+
+
+            Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
+            Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
+            Console.WriteLine("4 * 2 = " + proxy.Mul(4, 2));
+            Console.WriteLine("4 / 2 = " + proxy.Div(4, 2));
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IMath
+    {
+        double Add(double x, double y);
+        double Sub(double x, double y);
+        double Mul(double x, double y);
+        double Div(double x, double y);
+
+    }
+    
+    class Math : IMath
+    {
+        public double Add(double x, double y) { return x + y; }
+        public double Sub(double x, double y) { return x - y; }
+        public double Mul(double x, double y) { return x * y; }
+        public double Div(double x, double y) { return x / y; }
+
+    }
+    class MathProxy : IMath
+    {
+        private Math _math = new Math();
+
+        public double Add(double x, double y)
+        {
+            return _math.Add(x, y);
+        }
+
+        public double Sub(double x, double y)
+        {
+            return _math.Sub(x, y);
+        }
+
+        public double Mul(double x, double y)
+        {
+            return _math.Mul(x, y);
+        }
+
+        public double Div(double x, double y)
+        {
+            return _math.Div(x, y);
         }
     }
 }
